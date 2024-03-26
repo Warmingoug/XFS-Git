15e0fa709747f8579de0538f7d14f627864f63a8
Warming <warmingoug@gmail.com>
2024-03-26 11:14:40 +0800

    add print

--- XFS-Git/ViewController.swift
+++ XFS-Git/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name = "warming"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello \(name)")
     }
 
 
